e790ef3a6559b17d7d713c5380fab1a9738d8863
Nitin Pal <nitinkp1999@gmail.com>
2025-01-11 22:43:20 +0530

    update the code

---
+++ thank-you.html.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="refresh" content="3; url=index.php">
+	<title>Thank You</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<style>
+		@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap');
+		@import url('https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap');
+
+		.thankyouContainer {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			background: #002e3d url(images/banner.jpg) center no-repeat;
+			background-size: cover;
+		}
+
+		.thankyouBox {
+			width: 50%;
+			padding: 20px 70px 70px;
+			border-radius: 10px;
+			background:rgb(255 255 255);
+			text-align: center;
+			/*backdrop-filter: blur(5px) brightness(1.5);*/
+			color: #191310;
+		}
+
+		.thankyouBox h1 {
+			font: 400 6vw/1 'DM Serif Display', serif;
+		}
+
+		.thankyouBox .img-fluid {
+			margin: 0 auto 30px;
+			width:150px;
+		}
+	</style>
+</head>
+
+<body>
+	<div class="thankyouContainer vw-100 vh-100">
+		<div class="thankyouBox shadow-lg">
+			<img src="images/email.png" class="img-fluid" alt="Thank you" />
+			<h1>Thank You</h1>
+			<h4>Thank you for reaching out. <br />
+				Our team will get in touch with you soon.</h5>
+		</div>
+	</div>
+</body>
+
+</html>
